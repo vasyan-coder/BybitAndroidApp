5bc3d2cd4d645eba5fd6cbde0a921299986fd95c
vasyan <vasyan.coder@gmail.com>
2024-01-20 11:44:53 +0300

    feat: added ktor plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jlleitschuh.gradle.ktlint)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
